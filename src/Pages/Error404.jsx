ab6923d6f754deabc254528680efe70478537de1
mdsabir07 <sabiruli@gmail.com>
2025-07-09 22:15:57 +0600

    Created a dynamic error page and setup the route globally

--- src/Pages/Error404.jsx
+++ src/Pages/Error404.jsx
@@ -1,18 +1,18 @@
 import { Link, useLocation } from 'react-router';
-import img404 from '../../../assets/404.jpg';
+// import img404 from '../../src/assets/';
 
 const Error404 = () => {
     const location = useLocation();
     return (
         <div className='max-w-[1280px] mx-auto px-6 lg:px-0 grid gap-5 pt-8 pb-14 text-center justify-center'>
             <figure>
-                <img src={img404} alt="404" className='rounded-xl w-full max-w-4xl' />
+                {/* <img src={img404} alt="404" className='rounded-xl w-full max-w-4xl' /> */}
             </figure>
             <h1 className='text-red-500 font-extrabold text-5xl'>404 - Page Not found!</h1>
             <p className="text-xl md:text-2xl my-3">
-                Opps! The page <code className='clrPrimary font-bold'>{location.pathname}</code> you're looking for doesn't exist.
+                Opps! The page <code className='clr-secondary font-bold'>{location.pathname}</code> you're looking for doesn't exist.
             </p>
-            <Link to="/" className="px-8 py-3 text-lg font-semibold rounded bg-clrPrimary hover:text-white">Go to Home</Link>
+            <Link to="/" className="px-8 py-3 btn text-lg font-semibold rounded clr-primary-bg">Go to Home</Link>
         </div>
     );
 };
