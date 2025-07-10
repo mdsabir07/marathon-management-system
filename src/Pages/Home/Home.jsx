48fe4e6c1610ba56769786518a9d304178f6d461
mdsabir07 <sabiruli@gmail.com>
2025-07-10 15:15:04 +0600

    Created a newsletter section on the homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import HeroSlider from './Shared/HeroSlider';
 import MarathonCard from './Shared/MarathonCard';
 import CountdownTimer from './Shared/CountdownTimer';
 import UpcomingMarathons from './Shared/UpcomingMarathons';
+import NewsLetter from './Shared/NewsLetter';
 
 const Home = () => {
     const marathons = useLoaderData();
@@ -10,20 +11,24 @@ const Home = () => {
     return (
         <>
             <HeroSlider />
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-11/12 mx-auto">
-                {
-                    marathons.map(marathon => (
-                        <MarathonCard key={marathon._id} marathon={marathon} />
-                    ))
-                }
+            <div className="w-11/12 mx-auto">
+                <h2 className="text-3xl sm:text-5xl font-bold mb-5 sm:mb-8 clr-secondary text-center">Trending marathons</h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {
+                        marathons.map(marathon => (
+                            <MarathonCard key={marathon._id} marathon={marathon} />
+                        ))
+                    }
+                </div>
             </div>
 
             <UpcomingMarathons />
 
             <section className="my-10 w-11/12 mx-auto">
-                <h2 className="text-3xl font-bold mb-5 sm:mb-8 clr-secondary text-center">Marathon Countdown</h2>
+                <h2 className="text-3xl sm:text-5xl font-bold mb-5 sm:mb-8 clr-secondary text-center">Marathon Countdown</h2>
                 <CountdownTimer marathonDate={marathonDate} />
             </section>
+            <NewsLetter />
         </>
     );
 };
--- src/Pages/Home/Shared/UpcomingMarathons.jsx
+++ src/Pages/Home/Shared/UpcomingMarathons.jsx
@@ -33,7 +33,7 @@ const UpcomingMarathons = () => {
     return (
         <section className="py-12 w-11/12 mx-auto">
             <div className="px-4">
-                <h2 className="text-3xl font-bold text-center mb-10">
+                <h2 className="text-3xl sm:text-5xl clr-secondary font-bold text-center mb-10">
                     Upcoming Marathon Events
                 </h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
