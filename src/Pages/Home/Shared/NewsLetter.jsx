0378fbbe263e959450f923aa0216467a411dd807
mdsabir07 <sabiruli@gmail.com>
2025-07-10 15:19:03 +0600

    Fixed responsive issue for the newsletter

--- src/Pages/Home/Shared/NewsLetter.jsx
+++ src/Pages/Home/Shared/NewsLetter.jsx
@@ -4,10 +4,10 @@ const NewsLetter = () => {
     return (
         <div className="p-6 py-12 my-8 sm:my-12 w-11/12 mx-auto border border-gray-600 rounded-md">
             <div className="flex flex-col lg:flex-row items-center justify-between">
-                <div>
+                <div className='text-center sm:text-left mb-5 sm:mb-0'>
                     <h2 className="text-3xl sm:text-5xl tracking-tighter clr-secondary font-bold">🏃‍♂️ Streamline Your Marathon Events with Ease!
                     </h2>
-                    <div className="mt-5 clr-primary font-semibold text-lg">
+                    <div className="mt-3 sm:mt-5 clr-primary font-semibold text-lg">
                         Introducing the Ultimate Marathon Management System
                     </div>
                 </div>
